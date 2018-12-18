e757cafba65ffddcb12e598573a8876da90b9a4a
asiz <alexsneezy@gmail.com>
2018-12-18 23:16:01 +0700

    title test added

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,5 +1,7 @@
 package ru.deliveryClub;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,8 +11,11 @@ public class FirstTest {
     public void firstTest() {
         System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver.exe" );
         ChromeDriver driver = new ChromeDriver();
-
         driver.get("http://deliveryclub.ru");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("«Delivery Club» — круглосуточная доставка из любимых ресторанов города"));
+
+        driver.quit();
 
 
     }
