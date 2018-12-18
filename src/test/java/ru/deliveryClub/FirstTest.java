1190474de8fdf2936d55ad2a80261ee5f08dbf44
asiz <alexsneezy@gmail.com>
2018-12-19 00:11:47 +0700

    before/after anotations

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,22 +1,37 @@
 package ru.deliveryClub;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
+    public ChromeDriver driver;
 
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("test start");
+    }
     @Test
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver", "/chromedriver/chromedriver.exe" );
-        ChromeDriver driver = new ChromeDriver();
+
         driver.get("http://deliveryclub.ru");
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("«Delivery Club» — круглосуточная доставка из любимых ресторанов города"));
+    }
+    @Test
+    public void firstTest2() {
 
+        driver.get("http://deliveryclub.ru");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("«Delivery Club» — круглосуточная доставка из любимых ресторанов города"));
+    }
+    @After
+    public void close() {
+        System.out.println("test close");
         driver.quit();
-
-
     }
 }
